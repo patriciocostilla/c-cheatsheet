db97e0ca2f7e99bacdafa9b4020f333a07f38635
Patricio Costilla <patriciocostilla@gmail.com>
2020-02-28 19:08:20 -0300

    updating sizeof

--- sizeof.c
+++ sizeof.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  // sizeof() muestra el tamaño en bytes
-  printf("int: %ld \n", sizeof(int));
-  printf("float: %ld \n", sizeof(float));
-  printf("double: %ld \n", sizeof(double));
-  printf("char: %ld \n", sizeof(char));
+    // sizeof() muestra el tamaño en bytes
+    printf("int: %ld \n", sizeof(int));
+    printf("float: %ld \n", sizeof(float));
+    printf("double: %ld \n", sizeof(double));
+    printf("char: %ld \n", sizeof(char));
 
   return 0;
 }
