5e9b3e3de656f1bffa9b70ae6387612bfc07725c
Patricio Costilla <patriciocostilla@gmail.com>
2020-02-28 20:06:01 -0300

    updating scanf

--- scanf.c
+++ scanf.c
@@ -5,4 +5,10 @@ int main() {
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     printf("Sum = %d\n", a + b);
+
+    return 0;
 }
+/*
+scanf() stops reading as soon as it encounters a space, 
+so text such as "Hello World" is two separate inputs for scanf().
+*/
