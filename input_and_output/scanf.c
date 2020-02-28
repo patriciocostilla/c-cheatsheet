a51623467fc76d016966e6f255ab883adedfc12d
Patricio Costilla <patriciocostilla@gmail.com>
2020-02-28 20:20:40 -0300

    updating scanf

--- input_and_output/scanf.c
+++ input_and_output/scanf.c
@@ -11,4 +11,16 @@ int main() {
 /*
 scanf() stops reading as soon as it encounters a space, 
 so text such as "Hello World" is two separate inputs for scanf().
+Format specifiers begin with a percent sign % and are used to assign values to corresponding arguments after the control string. 
+Blanks, tabs, and newlines are ignored.
+A format specifier can include several options along with a conversion character:
+%[*][max_field]conversion character
+The optional * will skip the input field.
+The optional max_width gives the maximum number of characters to assign to an input field.
+The conversion character converts the argument, if necessary, to the indicated type:
+d decimal
+c character
+s string
+f float
+x hexadecimal
 */
