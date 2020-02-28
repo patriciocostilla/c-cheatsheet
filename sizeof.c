4c4b62baca6c8b27b66978f7c3ec31d3264ccf0b
Patricio Costilla <patriciocostilla@gmail.com>
2020-02-28 18:47:57 -0300

    adding sizeof program

---
+++ sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  // sizeof() muestra el tamaño en bytes
+  printf("int: %ld \n", sizeof(int));
+  printf("float: %ld \n", sizeof(float));
+  printf("double: %ld \n", sizeof(double));
+  printf("char: %ld \n", sizeof(char));
+
+  return 0;
+}
