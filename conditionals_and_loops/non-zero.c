eb507cc5ea0c8edc1e960bd47fa8e4d541bb2d89
Patricio Costilla <patriciocostilla@gmail.com>
2020-03-09 09:44:30 -0300

    non-zero

---
+++ conditionals_and_loops/non-zero.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int in_stock = 20;
+    if (in_stock) { //An expression that evaluates to a non-zero value is considered true.
+        printf("Order received.\n");
+    }
+    return 0;
+}
