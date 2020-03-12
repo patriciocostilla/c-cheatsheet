2dda7e6c18de87ec84a23187a5e87379bf143e36
Patricio Costilla <patriciocostilla@gmail.com>
2020-03-12 12:29:08 -0300

    pointers

---
+++ pointers/pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int j = 63;
+    int *p = NULL;
+    p = &j;
+
+    printf("The address of j is %x\n", &j);
+    printf("p contains address %x\n", p);
+    printf("The value of j is %d\n", j);
+    printf("p is pointing to the value %d\n", *p);
+
+    return 0;
+}
