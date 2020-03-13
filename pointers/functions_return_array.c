e1efdfdb8317b62dcb2883ef5d4d55a3b5ab25d4
Patricio Costilla <patriciocostilla@gmail.com>
2020-03-13 09:45:52 -0300

    functions that return an array

---
+++ pointers/functions_return_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int *get_evens();
+
+int main() {
+    int *a; // Declare a pointer to store the array returned
+    a = get_evens();
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", a[i]);
+    }
+    return 0;
+}
+
+int *get_evens() {
+    static int nums[5]; // Declare static to pass out of function
+    int even = 0;
+    for (int i = 0; i < 5; i++) {
+        even = even + 2;
+        nums[i] = even;
+    }
+    return nums;
+}
